b47529ff21c0ac1474d728a56afd107efa7981ff
Marco <marcopa84@hotmail.it>
2020-02-06 11:29:36 +0100

    Search OK!

--- js/script.js
+++ js/script.js
@@ -8,4 +8,38 @@ $(document).ready(function() {
 function filmSearch() {
   var searchVal = $('#search_val').val().toLowerCase();
   console.log(searchVal);
+  $.ajax(
+    {
+      url : 'https://api.themoviedb.org/3/search/movie?api_key=f4cb5d5e967d170cb7a067b541e15041',
+      method: 'GET',
+      data : {
+        query : searchVal,
+      },
+      success : function (data) {
+        var filmArray = data.results;
+        filmPrint(filmArray);
+      },
+      error : function(errors) {
+        alert('errore', errors);
+      }
+    }
+  );
+}
+
+function filmPrint(array) {
+  var source = document.getElementById("entry_film").innerHTML;
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < array.length; i++) {
+    var film = array[i];
+
+    var context = {
+      title: film.title,
+      original_title: film.original_title,
+      original_language: film.original_language,
+      vote_average: film.vote_average
+     };
+    var html = template(context);
+    $('.films_list').append(html);
+    console.log(film);
+  }
 }
